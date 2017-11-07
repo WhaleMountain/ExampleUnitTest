8feb3fedc4964242e01e54b96b07d65f4db7281a
Ken Miyahira <e175733@ie.u-ryukyu.ac.jp>
2017-11-07 09:54:49 +0900

    add test code for Enemy.attack()

---
+++ src/main/jp/ac/uryukyu/ie/e175733/LivingThing.java
@@ -0,0 +1,20 @@
+package jp.ac.uryukyu.ie.e175733;
+
+public class LivingThing {
+    String name;
+    int hitPoint;
+    int attack;
+    boolean dead;
+    public LivingThing(String name,int hitPoint,int attack){
+
+    }
+    public boolean isDead(){
+
+    }
+    public String getName(){
+
+    }
+    public void attack(LivingThing opponent){
+        
+    }
+}
