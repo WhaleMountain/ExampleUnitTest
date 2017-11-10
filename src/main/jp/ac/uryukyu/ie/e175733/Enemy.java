1e1591106202236207c59ea8f2195b49f22dbe53
Ken Miyahira <e175733@ie.u-ryukyu.ac.jp>
2017-11-10 15:14:00 +0900

    add test code for Enemy.attack()

--- src/main/jp/ac/uryukyu/ie/e175733/Enemy.java
+++ src/main/jp/ac/uryukyu/ie/e175733/Enemy.java
@@ -47,12 +47,10 @@ public class Enemy {
      * @param hero 攻撃対象
      */
     public void attack(Hero hero){
-        if(hitPoint>0&&!hero.dead) {
+        if(this.dead==false) {
             int damage = (int) (Math.random() * attack);
             System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, hero.getName(), damage);
             hero.wounded(damage);
-        }else if(hitPoint<0){
-            dead = true;
         }
     }
 
