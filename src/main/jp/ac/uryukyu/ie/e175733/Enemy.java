21db71705420fa08fee3d3fee42aad6f0e5ed4db
Ken Miyahira <e175733@ie.u-ryukyu.ac.jp>
2017-11-10 14:53:10 +0900

    add test code for Enemy.attack()

--- src/main/jp/ac/uryukyu/ie/e175733/Enemy.java
+++ src/main/jp/ac/uryukyu/ie/e175733/Enemy.java
@@ -47,7 +47,7 @@ public class Enemy {
      * @param hero 攻撃対象
      */
     public void attack(Hero hero){
-        if(hitPoint>0&&hero.dead) {
+        if(hitPoint>0&&!hero.dead) {
             int damage = (int) (Math.random() * attack);
             System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, hero.getName(), damage);
             hero.wounded(damage);
--- src/main/jp/ac/uryukyu/ie/e175733/LivingThing.java
+++ src/main/jp/ac/uryukyu/ie/e175733/LivingThing.java
@@ -4,23 +4,26 @@ public class LivingThing {
     String name;
     int hitPoint;
     int attack;
-    boolean dead=true;
+    boolean dead;
     public LivingThing(String name,int hitPoint,int attack){
         this.name=name;
         this.hitPoint=hitPoint;
         this.attack=attack;
+        isDead();
     }
     public boolean isDead(){
-
+        dead=false;
     }
     public String getName(){return name;}
 
     public void attack(LivingThing opponent){
-
+        int damage=(int)(Math.random()*attack);
+        System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", this.name, opponent.name, damage);
+        opponent.wounded(damage);
     }
     public void wounded(int damage){
         hitPoint-=damage;
-        if(hitPoint>0) {
+        if(hitPoint<0) {
             dead=true;
             System.out.printf("%sは倒れた。\n", this.name);
         }
